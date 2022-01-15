268a252519645ec979dff127fe730658bf4fde0b
HwaHyang <me@hwahyang.space>
2022-01-15 11:58:00 +0900

    Add files via upload

---
+++ 20220115/20220115/1/print.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class print
+{
+    /*static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("{0}/{1}/{2}", 2022, 01, 15);
+
+        Console.Write("이름을 입력하세요: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("당신의 이름은 {0} 입니다.", name);
+    }*/
+}
---
+++ 20220115/20220115/1/variable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20220115._1
+{
+    class variable
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 1.5; // 16-digit
+            float c = 1.3f; // 7-digit
+            string d = "asdf";
+            char e = 'A';
+
+
+        }
+    }
+}

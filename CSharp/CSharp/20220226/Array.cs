c80c3755583a5e0f8457b7694a94cf92149eee98
HwaHyang <me@hwahyang.space>
2022-02-26 20:19:04 +0900

    20220226

---
+++ CSharp/CSharp/20220226/Array.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp._20220226
+{
+	class Array
+	{
+		static void PrintIntArray(int[] arr, bool changeLine = true)
+		{
+			Console.Write("{ ");
+			for (int i = 0; i < arr.Length; i++)
+			{
+				Console.Write(arr[i]);
+				if (i < arr.Length - 1)
+					Console.Write(", ");
+			}
+			Console.Write(" }");
+
+			if (changeLine)
+				Console.Write("\n");
+		}
+
+		static void Main_20220226_1(string[] args)
+		{
+			/* 선언 */
+			int[] a;
+
+			/* 초기화 */
+			a = new int[10]; // 선언 후 초기화
+			int[] b = new int[10]; // 기본
+			int[] c = { 2, 4, 6, 8, 10, 12, 14 }; // C 스타일
+			int[] d = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // 대체 (기본 + C 스타일, 배열 길이 지정은 선택사항)
+			int[] e = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // 대체
+
+			int[] arr = { 2, 4, 6, 8, 10, 12, 14 };
+			PrintIntArray(arr);
+			arr[4] = 100;
+			PrintIntArray(arr);
+			Console.WriteLine("arr1의 길이: {0}\n", arr.Length);
+
+			int[] inputs = new int[5];
+			int sum = 0;
+			for (int i = 0; i < inputs.Length; i++)
+			{
+				Console.Write("{0}번 칸에 저장 할 수를 입력하세요: ", i);
+				inputs[i] = int.Parse(Console.ReadLine());
+				sum += inputs[i];
+			}
+			Console.Write("\n입력한 값은 다음과 같습니다: ");
+			PrintIntArray(inputs, false);
+			Console.WriteLine(" (총합 {0})", sum);
+		}
+	}
+}
---
+++ CSharp/CSharp/20220226/Test.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp._20220226
+{
+    class Test
+	{
+		static void ClearConsoleWhenEntered()
+        {
+			Console.Write("계속하려면 엔터를 입력하세요.");
+			Console.ReadLine();
+			Console.Clear();
+        }
+
+		static void PrintIntArray(int[] arr, bool changeLine = true)
+		{
+			Console.Write("{ ");
+			for (int i = 0; i < arr.Length; i++)
+			{
+				Console.Write(arr[i]);
+				if (i < arr.Length - 1)
+					Console.Write(", ");
+			}
+			Console.Write(" }");
+
+			if (changeLine)
+				Console.Write("\n");
+		}
+
+		static void Main(string[] args) //_20220226_2
+        {
+			int[] inputs1 = new int[10];
+			for (int i = 0; i < inputs1.Length; i++)
+			{
+				Console.Write("{0}번 칸에 저장 할 수를 입력하세요: ", i);
+				inputs1[i] = int.Parse(Console.ReadLine());
+			}
+			Console.Write("\n입력한 값은 다음과 같습니다: ");
+			PrintIntArray(inputs1);
+			Console.Write("\n");
+			ClearConsoleWhenEntered();
+
+			Console.Write("배열의 크기를 지정하세요: ");
+			int len = int.Parse(Console.ReadLine());
+			int[] inputs2 = new int[len];
+			for (int i = 0; i < inputs2.Length; i++)
+			{
+				Console.Write("{0}번 칸에 저장 할 수를 입력하세요: ", i);
+				inputs2[i] = int.Parse(Console.ReadLine());
+			}
+			Console.Write("\n입력한 값은 다음과 같습니다: ");
+			PrintIntArray(inputs2);
+			Console.Write("\n");
+			ClearConsoleWhenEntered();
+
+			Console.Write("input1의 값은 다음과 같습니다: ");
+			PrintIntArray(inputs1);
+			Console.Write("\n");
+
+			int[] inputs3 = new int[10];
+			inputs3 = inputs1;
+			Console.Write("복사된 input3의 값은 다음과 같습니다: ");
+			PrintIntArray(inputs3);
+			Console.Write("\n");
+		}
+    }
+}

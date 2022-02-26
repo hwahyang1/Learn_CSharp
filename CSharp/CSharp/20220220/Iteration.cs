019586c29c1d640609e3d574aee04d8dc36fff8a
HwaHyang <me@hwahyang.space>
2022-02-26 20:18:39 +0900

    20220220

---
+++ CSharp/CSharp/20220220/Iteration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp._20220220
+{
+	class Iteration
+	{
+		static void Main_20220220_1(string[] args)
+		{
+			int sum1 = 0;
+			for (int i = 1; i <=100; i++)
+			{
+				//Console.WriteLine("현재 {0}번째 반복 중입니다.", i);
+				sum1 += i;
+			}
+			Console.WriteLine("1~100까지의 합: {0}", sum1);
+
+			int sum2 = 0;
+			int j = 1;
+			while (j <= 100)
+			{
+				sum2 += j;
+				j++;
+			}
+			Console.WriteLine("1~100까지의 합: {0}", sum2);
+
+			int sum3 = 0;
+			int k = 1;
+			do
+			{
+				//Console.WriteLine("{0} = {0} + {1}", sum3, k);
+				sum3 += k;
+				k++;
+			} while (k <= 100);
+			Console.WriteLine("1~100까지의 합: {0}", sum3);
+
+			int l = 1;
+			for (; ; l++ )
+			{
+				Console.WriteLine(l);
+				if (l > 50000)
+					continue; // 조건식을 안거치고 for 문의 맨 위로 보냄
+			}
+
+			/*for (; ; )
+			{
+
+			}
+
+			while (true)
+			{
+
+			}
+
+			do
+			{
+
+			} while (true);*/
+		}
+	}
+}
---
+++ CSharp/CSharp/20220220/Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp._20220220
+{
+	class Test
+	{
+		static void Main_20220220_2(string[] args)
+		{
+			Console.Write("수를 입력하세요: ");
+			int Input = int.Parse(Console.ReadLine());
+			for (int i = 1; i <= 9; i++)
+			{
+				Console.WriteLine("{0} * {1} = {2}", Input, i, Input * i);
+			}
+			Console.WriteLine("");
+
+			int Count = 0;
+			int Sum = 0;
+			string List = "";
+			for (int i = 1; i <= Input; i++)
+			{
+				if (Input % i == 0)
+				{
+					Count++;
+					Sum += i;
+					List += i + " ";
+				}
+			}
+			Console.WriteLine("{0}의 약수의 개수는 {1}개 입니다: {2}(총합 {3})\n", Input, Count, List, Sum);
+
+			/* 최대공약수에서의 유클리드 호제법: https://ko.wikipedia.org/wiki/%EC%B5%9C%EB%8C%80%EA%B3%B5%EC%95%BD%EC%88%98#%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C_%ED%98%B8%EC%A0%9C%EB%B2%95 */
+
+			Console.Write("첫 번째 수를 입력하세요: ");
+			int Number1 = int.Parse(Console.ReadLine());
+			Console.Write("두 번째 수를 입력하세요: ");
+			int Number2 = int.Parse(Console.ReadLine());
+
+			int a = Number2; // 직전 수
+			int b = Number1 % Number2; // 나머지
+			int Temp;
+
+			while (b > 0)
+			{
+				//Console.WriteLine(b);
+				Temp = b;
+				b = a % b;
+				a = Temp;
+			}
+			Console.WriteLine("{0}와(과) {1}의 최대공약수는 {2}입니다.", Number1, Number2, a);
+		}
+	}
+}

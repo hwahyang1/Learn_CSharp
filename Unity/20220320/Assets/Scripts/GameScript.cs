90a080c30d0a2e9058d10ff25a348c260f295ebb
HwaHyang <me@hwahyang.space>
2022-03-21 00:04:12 +0900

    20220320

---
+++ Unity/20220320/Assets/Scripts/GameScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+	private void Start()
+	{
+		
+	}
+
+	private void Update()
+	{
+		Debug.Log("Info Log");
+		Debug.LogWarning("Warning Log");
+		Debug.LogError("Error Log");
+	}
+}
